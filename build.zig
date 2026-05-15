91a28a57936543bad67ec5563ec1b9400023b55b
Gealber <gealbermorales@gmail.com>
2026-05-15 20:48:53 +0200

    adding optimize flag

--- build.zig
+++ build.zig
@@ -8,9 +8,12 @@ const std = @import("std");
 // know when a step doesn't need to be re-run).
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const mod = b.addModule("skiplist", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
     const mod_tests = b.addTest(.{
